9129d42a55f520fa34549076271d56a2b2d0f6a1
amitguptaa9 <161274781+Amitencriss@users.noreply.github.com>
2024-06-01 15:09:28 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 exports.lambda_handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello, World!'),
+        body: JSON.stringify('Hello, World Amit!'),
     };
     return response;
 };
